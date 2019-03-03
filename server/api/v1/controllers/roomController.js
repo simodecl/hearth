aed2c365d2c0caca2ceb79c208882e0404374ef9
Simon Decloedt <decloedt.simon@gmail.com>
2019-03-03 16:29:55 +0100

    API Create room

--- server/api/v1/controllers/roomController.js
+++ server/api/v1/controllers/roomController.js
@@ -1,11 +1,23 @@
 const serviceAccount = require('../../../config/firebase.json')
+const admin = require('firebase-admin')
+const errorHandler = require('./../utilities/errorHandler')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://hearth-40878.firebaseio.com"
+    databaseURL: 'https://hearth-40878.firebaseio.com'
 });
 
 const db = admin.firestore()
-exports.create_room = (res, req, next) => {
-
+exports.create_room = (req, res, next) => {
+    //Generate random 4 digit ID
+    const random = Math.floor(1000 + Math.random() * 9000);
+    console.log(random);
+    const newRoom = db.collection('rooms').doc(random.toString()).set({
+        code: random
+    })
+    if (newRoom) {
+        return res.json(random)
+    } else {
+        return errorHandler.handleAPIError(505, 'Could not create a new room', next);  
+    }
 }
--- server/api/v1/routes.js
+++ server/api/v1/routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const roomController = require('./controllers/roomController')
 
-router.post('rooms', roomController.create_room);
+router.post('/rooms', roomController.create_room);
 
 module.exports = router;
