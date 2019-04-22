a099f7724f83c1067a930fb9735d84d7bc254d71
Simon Decloedt <decloedt.simon@gmail.com>
2019-04-22 15:43:36 +0200

    heroku

--- server/config/firestore.js
+++ server/config/firestore.js
@@ -5,7 +5,7 @@ admin.initializeApp({
         "type": "service_account",
         "project_id": "hearth-40878",
         "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": "firebase-adminsdk-90nnn@hearth-40878.iam.gserviceaccount.com",
         "client_id": "101641751707903439433",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
